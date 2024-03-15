03a77723094887ccfe8cf0e1e370cf2ad247f3c3
Vinuk Ranaweera <vinukranaweera@gmail.com>
2024-03-15 17:44:13 -0400

    fixed logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Containter from "./components/Container";
 import SearchBar from './components/SearchBar';
 import PokemonCardList from './components/PokemonCardList';
 import PokeButton from './components/PokeButton';
+import PokeLogo from './image/pokelogo.png';
 
 function App() {
   const {pokemons, selectPks, selectPksUpdate, resultPks} = usePokemonInfo()
   return (
             <div className="bg-[#3466AF] max-h-screen">
               <div className="flex flex-col h-screen justify-center items-center">{/*flex wrapper*/}
-                <img src = "https://drive.google.com/uc?export=view&id=16JW-S_WDxEs8DXxp9ZM2ik_DnrakUffP" className= "w-[200px] mx-auto h-[15%]"/>
+                <img src = {PokeLogo} className= "w-[200px] mx-auto h-[15%]"/>
                 <Containter className= "px-5 w-[90%] h-[8%]"><h1 className="text-center font-semibold">Build your best Pokémon team and determine your winning rates!</h1></Containter>
                 <div className="flex flex-row h-[75%] w-[90%]">
                   <Containter className = "w-1/3 flex flex-col gap-y-2">
